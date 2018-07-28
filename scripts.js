c746c3879342253fe4dc8e0b9bc3dee715a1fcf9
sharnajh <sharnajhossain@gmail.com>
2018-07-28 16:13:01 -0400

    more changes

--- scripts.js
+++ scripts.js
@@ -4,15 +4,21 @@ let colorA = pickerA.value,
 colorB = pickerB.value;
 let hexTextA = document.getElementById("hexTextA"),
 hexTextB = document.getElementById("hexTextB");
+let preview = document.getElementById("preview"),
+see = document.getElementById("see");
 
 hexTextA.innerHTML = colorA;
 hexTextB.innerHTML = colorB;
 
+
 pickerA.addEventListener("input", function() {
     hexTextA.innerHTML = pickerA.value;
+    document.body.style = `background: linear-gradient(to bottom left, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%`;
 }, false);
 
 pickerB.addEventListener("input", function() {
     hexTextB.innerHTML = pickerB.value;
+    document.body.style = `background: linear-gradient(to bottom left, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%`;
 }, false);
+
 
